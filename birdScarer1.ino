3f43dd2168f0f605cdc1958edb7a8ffc721a3dab
MichaelRea-NZ <58958188+MichaelRea-NZ@users.noreply.github.com>
2023-03-12 15:35:31 +1300

    Add files via upload

---
+++ birdScarer1.ino
@@ -0,0 +1,55 @@
+int calibrationTime = 30;
+long unsigned int lowIn;
+long unsigned int pause = 5000;
+boolean lockLow = true;
+boolean takeLowTime;
+int pirPin = 7;
+int ledPin = 5;
+int soundPin = 9;
+
+void setup(){
+ Serial.begin(9600);
+ pinMode(pirPin , INPUT); 
+ pinMode(ledPin , OUTPUT);
+ pinMode(soundPin , OUTPUT);
+ digitalWrite(pirPin, LOW);
+ 
+ Serial.print("Calibrating sensor ");
+   for(int i= 0; i < calibrationTime; i ++){
+     Serial.print(".");
+     delay(1000);
+ }
+ Serial.println("Set");
+}
+
+void loop(){
+  if(digitalRead(pirPin) == HIGH){
+   digitalWrite(ledPin, HIGH);   
+   tone(soundPin, 2500);
+   if(lockLow){
+    Serial.println("------------------------------------");
+    Serial.print("motion detected at ");
+    Serial.print(millis()/1000 );
+    Serial.println(" seconds.");
+    delay(2250);
+   } 
+   takeLowTime = true;
+  }
+  if(digitalRead(pirPin) == LOW){
+   digitalWrite(ledPin, LOW);
+   noTone(soundPin);
+
+  if(takeLowTime){
+    lowIn - millis();
+    takeLowTime = false;
+  }
+  
+   
+   if(! takeLowTime && millis() - lowIn > pause){
+    lockLow = true;
+   delay(50); 
+   }
+  }
+}
+
+
---
+++ bird_scarer2.ino
@@ -0,0 +1,25 @@
+int LED = 13;
+int PIR = 2;
+
+void setup(){
+pinMode(LED, OUTPUT);
+pinMode(PIR, INPUT);
+Serial.begin(9600);
+}
+
+void loop(){
+  int sensorval = digitalRead(PIR);
+  
+ if (PIR == HIGH){
+  digitalWrite(LED, HIGH);
+  
+  delay(100);
+  Serial.print("Motion detected");
+ }
+ else{
+  digitalWrite(LED, LOW);
+ 
+
+ }
+}
+
